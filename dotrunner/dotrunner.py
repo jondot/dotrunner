269cbc92bfaf0199890ceae942f4349a63fb7ba9
Dotan Nahum <jondotan@gmail.com>
2018-04-29 09:59:19 +0300

    build

--- dotrunner/dotrunner.py
+++ dotrunner/dotrunner.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from docopt import docopt
-from os.path import expanduser, join, isdir, basename
+from os.path import join, isdir, basename
 import os.path
 from toolz.curried import pipe, filter, map, get, concat, mapcat, merge, assoc, first  # noqa
-from os import symlink
 import networkx as nx
 import delegator
 import yaml
@@ -14,13 +13,6 @@ from .version import VERSION
 
 
 class FileSystemIO(object):
-    def link(self, source, target):
-        try:
-            symlink(source, target)
-            print("{} -> {}".format(source, target))
-        except:  # noqa
-            print("(exists) {}".format(target))
-
     def yaml(self, f):
         try:
             with open(f) as fp:
@@ -44,19 +36,10 @@ class FileSystemIO(object):
     def ls(self, dir):
         return os.listdir(dir)
 
-    def glob(self, root, recursive=True):
-        # walk: root [dirs] [files] -> for each dir (tup.2), for
-        # each file (tup.1), we need to join with root
-        return pipe(
-            os.walk(expanduser(root)),
-            mapcat(lambda tup:
-                map(lambda f: join(tup[0], f))(concat([tup[2], tup[1]]))),
-            list) # noqa
-
 
 class DryRunIO(FileSystemIO):
-    def link(self, source, target):
-        print("[dry-run] {} -> {}".format(source, target))
+    def execute(self, cwd, cmd):
+        print('(dry) {} {} ({})'.format(datetime.today(), cmd, cwd))
 
 
 def dirs(dir, io):
